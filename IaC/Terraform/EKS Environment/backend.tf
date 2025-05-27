b86db0f409c1cc8cc48169e7840c56d0a165a6d6
Domonic Neal <domonicneal3@yahoo.com>
2025-05-27 00:38:33 -0500

    updated backend.tf with SSE and block public access
    updated backend.tf with SSE and block public access

--- IaC/Terraform/EKS Environment/backend.tf
+++ IaC/Terraform/EKS Environment/backend.tf
@@ -9,3 +9,23 @@ terraform {
   }
 
 }
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "backend_encryption" {
+  bucket = "devops-projects-eks-environments-state"
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public" {
+  bucket = "devops-projects-eks-environments-state"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
